2f91a69a871454f1e30d29ce1cecaa0e5419ccd0
unknown <erichung.94@gmail.com>
2019-05-02 16:49:41 -0500

    Initial Commit

---
+++ client/build/precache-manifest.1b9cdd7d48ddf2b61755399f43531fcf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e904894531fb341da5ff",
+    "url": "/static/js/main.910f8f77.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "e904894531fb341da5ff",
+    "url": "/static/css/main.c9984b32.chunk.css"
+  },
+  {
+    "revision": "e2b418f5e4c8eb5a53ece0f9cf30c766",
+    "url": "/index.html"
+  }
+];
